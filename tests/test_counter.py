0a3db4a128de5c583835eef8085a0118cbaad8e7
LilAttacker <105605091+LilAttacker@users.noreply.github.com>
2024-10-03 12:38:19 -0700

    Update test_counter.py
    Modified format to try and accord with black code format.
    
    Changes by chatgpt (https://chatgpt.com/share/66fef290-1f84-800f-ad59-f0220ec4472e)

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -28,76 +28,76 @@ def client():
 
 @pytest.mark.usefixtures("client")
 class TestCounterEndPoints:
-    """ Test cases for Counter-related endpoints """
+    """Test cases for Counter-related endpoints"""
 
     # Basic Test To Test Creating a Counter
     def test_create_a_counter(self, client):
-        """ It should create a counter """
-        result = client.post('/counters/foo')
+        """It should create a counter"""
+        result = client.post("/counters/foo")
         assert result.status_code == status.HTTP_201_CREATED
 
     def test_duplicate_a_counter(self, client):
-        """ It should return an error for duplicates """
-        result = client.post('/counters/bar')
+        """It should return an error for duplicates"""
+        result = client.post("/counters/bar")
         assert result.status_code == status.HTTP_201_CREATED
-        result = client.post('/counters/bar')
+        result = client.post("/counters/bar")
         assert result.status_code == status.HTTP_409_CONFLICT
 
     def test_update_a_counter(self, client):
-        """ It should update an existing counter """
+        """It should update an existing counter"""
         # Update Counter That Doesn't Exist
-        result = client.put('/counters/uac')
+        result = client.put("/counters/uac")
         assert result.status_code == status.HTTP_404_NOT_FOUND
 
         # Make Counter To Exist
-        result = client.post('/counters/uac')
+        result = client.post("/counters/uac")
         assert result.status_code == status.HTTP_201_CREATED
 
         # Check Counter Value
         assert COUNTERS["uac"] == 0
 
         # Update Counter
-        result = client.put('/counters/uac')
+        result = client.put("/counters/uac")
         assert result.status_code == status.HTTP_200_OK
 
         # Test Counter Value
         assert COUNTERS["uac"] == 1
 
     def test_get_a_counter(self, client):
-        """ It should get an existing counter """
+        """It should get an existing counter"""
         # Read Counter That Doesn't Exist
-        result = client.get('/counters/gac')
+        result = client.get("/counters/gac")
         assert result.status_code == status.HTTP_404_NOT_FOUND
 
         # Make Counter To Exist
-        result = client.post('/counters/gac')
+        result = client.post("/counters/gac")
         assert result.status_code == status.HTTP_201_CREATED
 
         # Retrieve Counter
-        result = client.get('/counters/gac')
+        result = client.get("/counters/gac")
         assert result.status_code == status.HTTP_200_OK
 
         # Read Counter
         assert COUNTERS["gac"] == 0
 
     def test_delete_a_counter(self, client):
-        """ It should delete a counter """
+        """It should delete a counter"""
         # Delete Counter That Doesn't Exist
-        result = client.delete('/counters/grave')
+        result = client.delete("/counters/grave")
         assert result.status_code == status.HTTP_404_NOT_FOUND
 
         # Make Counter To Exist
-        result = client.post('/counters/grave')
+        result = client.post("/counters/grave")
         assert result.status_code == status.HTTP_201_CREATED
 
         # Retrieve Counter to Prove Existence
-        result = client.get('/counters/grave')
+        result = client.get("/counters/grave")
         assert result.status_code == status.HTTP_200_OK
 
         # Delete Counter
-        result = client.delete('/counters/grave')
+        result = client.delete("/counters/grave")
         assert result.status_code == status.HTTP_204_NO_CONTENT
 
         # Try to Read Counter to Prove Nonexistence Again
-        result = client.get('/counters/grave')
+        result = client.get("/counters/grave")
         assert result.status_code == status.HTTP_404_NOT_FOUND
