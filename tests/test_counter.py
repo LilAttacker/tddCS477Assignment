e0b0a1b1f5b0dfa1545cc555f9e74fadfad53dd6
LilAttacker <105605091+LilAttacker@users.noreply.github.com>
2024-10-03 12:40:16 -0700

    Update test_counter.py
    Forgot some minor format changes and deleted some excess comments that did nothing.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,17 +1,3 @@
-"""
-Test Cases for Counter Web Service
-
-Create a service that can keep a track of multiple counters
-- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
-how to call the web service and assert what it should return.
-- The endpoint should be called /counters
-- When creating a counter, you must specify the name in the path.
-- Duplicate names must return a conflict error code.
-- The service must be able to update a counter by name.
-- The service must be able to read the counter
-"""
-
-
 import pytest
 
 # we need to import the unit under test - counter
@@ -21,9 +7,10 @@ from src.counter import app, COUNTERS
 # we need to import the file that contains the status codes
 from src import status
 
+
 @pytest.fixture()
 def client():
-  return app.test_client()
+    return app.test_client()
 
 
 @pytest.mark.usefixtures("client")
